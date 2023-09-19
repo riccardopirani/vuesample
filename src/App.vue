a7a96c0e2160752776ae2ced74348e6074f8f84f
riccardopirani <riccardo@viewsoftware.it>
2023-09-19 16:46:51 +0200

    First commit
    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo"
+       src="./assets/logo.png">
+  <UserComponent msg="Utente component" />
+</template>
+
+<script>
+import UserComponent from './components/UserComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    UserComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
