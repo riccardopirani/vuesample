7e9a541327d21770d167b3ea0207b4deae0b2944
riccardopirani <riccardo@viewsoftware.it>
2023-09-25 09:19:35 +0200

    Fix code

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,14 +1,28 @@
 <template>
 <div>
     <h1>{{ msg }}</h1>
+    <h2>ChartName: {{chartName}}</h2>
 </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
     name: "UserComponent",
     props: {
         msg: String,
     },
+    data () {
+    return {
+      info: null,
+      chartName: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => this.chartName=response.data.chartName)
+      console.log(this.info);
+  }
 };
 </script>
